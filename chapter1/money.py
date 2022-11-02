de486b3e3d574412b7c254d5cb8bc2c750eeeb3a
Keisuke Yoshigai <yoshigai_keisuke@retail-ai.jp>
2022-11-02 22:21:23 +0900

    [chapter 4] Modified test to compare just themselves, not by their amount

--- chapter1/money.py
+++ chapter1/money.py
@@ -3,6 +3,9 @@ class Dollar:
     def __init__(self, amount):
         self.amount = amount
 
+    def __eq__(self, __o):
+        return __o.amount == self.amount
+
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
 
