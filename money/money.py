2963429914434f525d032013f41d09dc22234420
Keisuke Yoshigai <yoshigai_keisuke@retail-ai.jp>
2022-11-04 08:02:57 +0900

    [chapter 6] Fix Dollar and Franc classes to inherit Money class

--- money/money.py
+++ money/money.py
@@ -1,26 +1,22 @@
-class Dollar:
-    def __init__(self, amount):
-        self.amount = amount
-
+class Money:
     def __eq__(self, __o):
         return __o.amount == self.amount
+
+    def equals(self, money):
+        return self.amount == money.amount
+
+
+class Dollar(Money):
+    def __init__(self, amount):
+        self.amount = amount
 
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
 
-    def equals(self, dollar):
-        return self.amount == dollar.amount
 
-
-class Franc:
+class Franc(Money):
     def __init__(self, amount):
         self.amount = amount
 
-    def __eq__(self, __o):
-        return __o.amount == self.amount
-
     def times(self, multiplier):
         return Franc(self.amount * multiplier)
-
-    def equals(self, dollar):
-        return self.amount == dollar.amount
--- money/money_test.py
+++ money/money_test.py
@@ -7,11 +7,13 @@ class TestMoney:
         assert money.Dollar(10) == five.times(2)
         assert money.Dollar(15) == five.times(3)
 
+        five = money.Franc(5)
+        assert money.Franc(10) == five.times(2)
+        assert money.Franc(15) == five.times(3)
+
     def test_equality(self):
         assert money.Dollar(5).equals(money.Dollar(5))
         assert not money.Dollar(5).equals(money.Dollar(6))
 
-    def test_franc_multiplication(self):
-        five = money.Franc(5)
-        assert money.Franc(10) == five.times(2)
-        assert money.Franc(15) == five.times(3)
+        assert money.Franc(5).equals(money.Franc(5))
+        assert not money.Franc(5).equals(money.Franc(6))
