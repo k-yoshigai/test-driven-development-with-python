37adda83ba882fe74818d29f1d2b08340a1f063b
Keisuke Yoshigai <yoshigai_keisuke@retail-ai.jp>
2022-11-08 07:52:41 +0900

    [chapter 20] Add tearDown mothod

--- xunit.py
+++ xunit.py
@@ -5,33 +5,32 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 
 class WasRun(TestCase):
     def setUp(self):
-        self.WasRun = None
-        self.WasSetup = 1
+        self.log = "setUp "
 
     def testMethod(self):
-        self.WasRun = 1
+        self.log += "testMethod "
+
+    def tearDown(self):
+        self.log += "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert self.test.WasRun
-
-    def testSetUp(self):
-        self.test.run()
-        assert self.test.WasSetup
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert "setUp testMethod tearDown " == test.log
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
